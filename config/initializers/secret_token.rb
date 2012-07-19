b07a056bf9e8f8169aa232a28909c62abe67b2b6
Carlos Ribeiro <carlos.ribeiro@carlosribeiro.(none)>
2012-07-18 22:25:45 -0300

    primeiro commit. + devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PagueARep::Application.config.secret_token = '9412561ee0b7f348fb7530222f8f0ce89e330005332d7770113d78c48622b483b069d3b7e0e2aa62e99651a7e0f248aa95158bee6b81e6ae9912b638c77d66d8'
